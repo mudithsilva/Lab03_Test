5d70a953217239528a549f753a8fada161795942
Mudith Chathuranga <mudithchathuranga@gmail.com>
2019-09-12 23:20:02 +0200

    file changes

--- R/euclidean_func.R
+++ R/euclidean_func.R
@@ -8,7 +8,7 @@
 #' euclidean(100, 1000)
 #' @description
 #' Euclid's algorithm, is an efficient method for computing the greatest common divisor (GCD) of two numbers, the largest number that divides both of them without leaving a remainder.
-#' @seealso \code{\link{https://en.wikipedia.org/wiki/Euclidean_algorithm}} for Euclidean algorithm
+#' @seealso https://en.wikipedia.org/wiki/Euclidean_algorithm for Euclidean algorithm
 
 euclidean <- function(a, b) {
   value_one <- a
