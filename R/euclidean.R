9c3ca4dc1bd7367145cde695f8bf770b12bc6c3e
Mudith Chathuranga <mudithchathuranga@gmail.com>
2019-09-12 23:59:36 +0200

    Fixed error files

---
+++ R/euclidean.R
@@ -0,0 +1,23 @@
+#' Find the greatest common divisor of two numbers.
+#'
+#' @param a A Numerical value
+#' @param b A Numerical Value
+#'
+#' @return Gratest common divior of given values
+#' @export
+#'
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+#'
+euclidean <- function(a, b) {
+  value_one <- a
+  value_two <- b
+
+  while(value_two != 0) {
+    reminder_value <- value_one %% value_two
+    value_one <- value_two
+    value_two <- reminder_value
+  }
+  return(value_one)
+}
